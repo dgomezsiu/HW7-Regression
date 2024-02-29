6fd721ce6858045af4abf9214b8dda7eb4e3b813
David Gomez Siu <dgomezsiu@berkeley.edu>
2024-02-29 14:57:24 -0800

    first test

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -11,10 +11,30 @@ This is not an exhaustive list.
 # Imports
 import pytest
 # (you will probably need to import more things here)
+import numpy as np
+import math
+from sklearn.linear_model import LogisticRegression as SklearnLogisticRegression
+import regression.utils
+import regression.logreg
+
 
 def test_prediction():
-	pass
+        X = np.array([[0], [2], [3]])
+        y = np.array([0, 1, 1])
 
+        # Your custom logistic regression
+        your_model = LogisticRegressor(num_feats=1)
+        your_model.train_model(X, y, X, y)
+        your_predictions = your_model.make_prediction(X)
+
+        # Scikit-learn logistic regression
+        sklearn_model = SklearnLogisticRegression(solver='liblinear')
+        sklearn_model.fit(X, y)
+        sklearn_predictions = sklearn_model.predict(X)
+
+        # Compare predictions
+        np.testing.assert_array_equal(your_predictions, sklearn_predictions)
+		
 def test_loss_function():
 	pass
 

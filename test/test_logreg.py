a9cb14d675e2af94490a4a6bfc7c3db6dd0fb8d4
David Gomez Siu <dgomezsiu@berkeley.edu>
2024-03-01 14:35:20 -0800

    Update test_logreg.py

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -86,4 +86,4 @@ def test_training():
 		my_model_2 = regression.logreg.LogisticRegressor(num_feats=1, max_iter=20)
 		my_model_2.train_model(X, y, X, y)
 
-		assert my_model_1.W != my_model_2.W
+		assert np.all(my_model_1.W != my_model_2.W)
